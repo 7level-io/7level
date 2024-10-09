65be84371a5fbd60546dcb55c536099020dfdc40
Jerry <jerrylalrinmawia@gmail.com>
2024-10-09 18:13:54 +0530

    contact link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,8 @@
-// src/components/Layout.js
 import React from "react"
-import "../css/Layout.css" // Importing the CSS for styles
-import LogoIcon from "../assets/images/7level v3.png" // Import the image
-import ProfileImage from "../assets/images/alvixedoodle.png" // Import the image
+import "../css/Layout.css"
+import LogoIcon from "../assets/images/7level v3.png"
+import ProfileImage from "../assets/images/alvixedoodle.png"
+import ContactFile from "../assets/contacts/contacts.vcf"
 
 const Layout = () => {
   return (
@@ -18,10 +18,14 @@ const Layout = () => {
       </div>
 
       <div className="icon-row">
-        <div className="icon-item dark-bg one-row-item">
+        <a
+          href={ContactFile}
+          download
+          className="icon-item dark-bg one-row-item"
+        >
           <i className="fa fa-user-plus"></i>{" "}
           {/* Assuming you want to use Font Awesome for icons */}
-        </div>
+        </a>
         <div className="icon-item big-icon purple-bg">
           <i className="fa fa-clock"></i>
         </div>
@@ -30,7 +34,6 @@ const Layout = () => {
         </div>
       </div>
 
-      {/* Footer */}
       <div className="footer">
         <div className="footer-icon">
           <img src={LogoIcon} alt="Footer Icon" className="footer-icon" />
