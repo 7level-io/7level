b1016e286f29d270b35cbaa518c54c692416688c
Jerry <jerrylalrinmawia@gmail.com>
2024-10-10 23:24:46 +0530

    homepage styling

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "../css/home.css"
 
 const Home = () => {
-  return <Link to="/alvin">Alvin</Link>
+  return (
+    <div id="homepage">
+      <Link to="/alvin">Alvin</Link>
+    </div>
+  )
 }
 
 export default Home
