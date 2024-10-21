2f4df1c3c5fa015ce59a3b95a0f360fa37c1b1a0
Jerry <jerrylalrinmawia@gmail.com>
2024-10-21 22:40:28 +0530

    Changed MeetingScheduler to a embbed Popup instead of a link

--- src/components/MeetingScheduler.jsx
+++ src/components/MeetingScheduler.jsx
@@ -1,13 +1,38 @@
 import React from "react"
 
+// const MeetingScheduler = () => {
+//   return (
+//     <a
+//       href="https://cal.com/alvixe/7level"
+//       className="icon-item big-icon purple-bg schedule"
+//     >
+//       <i className="fa fa-clock"></i>
+//     </a>
+//   )
+// }
+
+import { getCalApi } from "@calcom/embed-react"
+import { useEffect } from "react"
 const MeetingScheduler = () => {
+  useEffect(() => {
+    ;(async function () {
+      const cal = await getCalApi({ namespace: "7level" })
+      cal("ui", {
+        styles: { branding: { brandColor: "#000000" } },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      })
+    })()
+  }, [])
   return (
-    <a
-      href="https://cal.com/alvixe/7level"
+    <button
+      data-cal-namespace="7level"
+      data-cal-link="alvixe/7level"
+      data-cal-config='{"layout":"month_view"}'
       className="icon-item big-icon purple-bg schedule"
     >
       <i className="fa fa-clock"></i>
-    </a>
+    </button>
   )
 }
 
