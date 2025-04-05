4e4ce674b8b09047bfcc075b313234c5e3579267
Jerry <jerrylalrinmawia@gmail.com>
2025-04-05 16:31:06 +0530

    Changed cal scheduler to a link instead of embedded

--- src/components/MeetingScheduler.jsx
+++ src/components/MeetingScheduler.jsx
@@ -1,39 +1,39 @@
 import React from "react"
 
-// const MeetingScheduler = () => {
-//   return (
-//     <a
-//       href="https://cal.com/alvixe/7level"
-//       className="icon-item big-icon purple-bg schedule"
-//     >
-//       <i className="fa fa-clock"></i>
-//     </a>
-//   )
-// }
-
-import { getCalApi } from "@calcom/embed-react"
-import { useEffect } from "react"
 const MeetingScheduler = () => {
-  useEffect(() => {
-    ;(async function () {
-      const cal = await getCalApi({ namespace: "7level" })
-      cal("ui", {
-        styles: { branding: { brandColor: "#000000" } },
-        hideEventTypeDetails: false,
-        layout: "month_view",
-      })
-    })()
-  }, [])
   return (
-    <button
-      data-cal-namespace="7level"
-      data-cal-link="alvixe/7level"
-      data-cal-config='{"layout":"month_view"}'
+    <a
+      href="https://cal.com/alvixe/7level"
       className="icon-item big-icon purple-bg schedule"
     >
       <i className="fa fa-clock"></i>
-    </button>
+    </a>
   )
 }
+
+// import { getCalApi } from "@calcom/embed-react"
+// import { useEffect } from "react"
+// const MeetingScheduler = () => {
+//   useEffect(() => {
+//     ;(async function () {
+//       const cal = await getCalApi({ namespace: "7level" })
+//       cal("ui", {
+//         styles: { branding: { brandColor: "#000000" } },
+//         hideEventTypeDetails: false,
+//         layout: "month_view",
+//       })
+//     })()
+//   }, [])
+//   return (
+//     <button
+//       data-cal-namespace="7level"
+//       data-cal-link="alvixe/7level"
+//       data-cal-config='{"layout":"month_view"}'
+//       className="icon-item big-icon purple-bg schedule"
+//     >
+//       <i className="fa fa-clock"></i>
+//     </button>
+//   )
+// }
 
 export default MeetingScheduler
